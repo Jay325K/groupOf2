e13a042f4f98c81a1986d3b08915b9039f7b13b7
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-05-01 11:34:11 +0900

    Delete header directory

--- header/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".gnb").mouseenter(function () {
-        $(".sub_menu").slideDown();
-    });
-    $(".gnb").mouseleave(function () {
-        $(".sub_menu").slideUp();
-    });
-});
