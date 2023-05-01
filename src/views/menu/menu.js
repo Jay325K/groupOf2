96c4bbb180daf243e44ea32af8461d3185530772
chocoro99 <lie202319@naver.com>
2023-05-01 17:56:04 +0900

    feat : 음료 페이지 이동, 음료 종류 별 출력 구현

--- src/views/menu/menu.js
+++ src/views/menu/menu.js
@@ -1,7 +1,15 @@
 const drinkPage = document.querySelector(".drink_page");
 const drinkList = document.querySelector(".drink_list");
 const category = document.querySelector(".content_category");
-console.log(category);
+const checkBox = document.querySelectorAll(".content_category > label");
+const categoryArray = [
+  "coffee/decaffeine",
+  "latte/choco/tea",
+  "hollyccino/crush",
+  "smoothie/juice/sparkling",
+];
+let drinkListPrint = [];
+let categoryChecked = [true, false, false, false, false];
 let pageNum = "1";
 
 const getMenu = () => {
@@ -9,53 +17,87 @@ const getMenu = () => {
   return response.then((res) => res.json());
 };
 
-const drinkPrintExec = async (num) => {
+const drinkPrintExec = async () => {
   try {
     const list = await getMenu();
-    const category = [
-      "coffee/decaffeine",
-      "latte/choco/tea",
-      "hollyccino/crush",
-      "smoothie/juice/sparkling",
-    ];
-    const allDrink = [
-      ...list["drink"][category[0]],
-      ...list["drink"][category[1]],
-      ...list["drink"][[category[2]]],
-      ...list["drink"][[category[3]]],
-    ];
-    const coffeeDecaffeine = [...list["drink"][category[0]]];
-    const latteChocoTea = [...list["drink"][category[1]]];
-    const hollyccinoCrush = [...list["drink"][category[2]]];
-    const smoothieJuiceSpacling = [...list["drink"][category[3]]];
-    if (!num || pageNum == num) {
-      drinkPagePrint(allDrink, num);
+
+    if (!pageNum || pageNum == pageNum) {
+      drinkPagePrint(list);
     }
   } catch (error) {
     console.log(error);
   }
 };
+const drinkPagePrint = (list) => {
+  drinkListPrint = [];
+  drinkList.innerHTML = "";
+  drinkPage.innerHTML = "";
 
-const drinkPagePrint = (list, page) => {
-  for (let i = (parseInt(page) - 1) * 20; i < 20 * page; i++) {
-    drinkList.innerHTML += `<li><a href="#none"><img src="${list[i].image}" alt=""><span>${list[i].name}</span></a></li>`;
-    if (i + 1 == list.length) {
-      break;
+  for (let i = 0; i < 5; i++) {
+    if (categoryChecked[0] == true) {
+      drinkListPrint = [
+        ...list["drink"][categoryArray[0]],
+        ...list["drink"][categoryArray[1]],
+        ...list["drink"][categoryArray[2]],
+        ...list["drink"][categoryArray[3]],
+      ];
+    } else if (categoryChecked[i]) {
+      drinkListPrint = [
+        ...drinkListPrint,
+        ...list["drink"][categoryArray[i - 1]],
+      ];
+    }
+  }
+  const pageCount = Math.ceil(drinkListPrint.length / 20);
+
+  for (let i = 1; i <= pageCount; i++) {
+    drinkPage.innerHTML += `<span>${i}</span>`;
+  }
+
+  for (let i = (parseInt(pageNum) - 1) * 20; i < 20 * pageNum; i++) {
+    if (!drinkListPrint[i]) {
+      drinkList.innerHTML += `<li class="blank_item"></li>`;
+    } else if (drinkListPrint[i]) {
+      drinkList.innerHTML += `<li><a href="#none"><img src="${drinkListPrint[i].image}" alt=""><span>${drinkListPrint[i].name}</span></a></li>`;
     }
   }
 };
 
 window.addEventListener("DOMContentLoaded", () => {
   if (window.location.pathname.includes("drink")) {
-    drinkPrintExec(pageNum);
+    drinkPrintExec();
   }
 });
 
 drinkPage.addEventListener("click", (e) => {
-  console.log("click");
   if (pageNum !== e.target.innerHTML && e.target.localName == "span") {
     pageNum = e.target.innerHTML;
-    drinkList.innerHTML = "";
-    drinkPrintExec(pageNum);
+    drinkPrintExec();
   }
+});
+category.addEventListener("change", () => {
+  pageNum = 1;
+
+  if (
+    checkBox[0].lastElementChild.checked == true &&
+    categoryChecked[0] == true
+  ) {
+    checkBox[0].lastElementChild.checked = false;
+    categoryChecked[0] = false;
+  } else if (
+    checkBox[0].lastElementChild.checked == true &&
+    categoryChecked[0] == false
+  ) {
+    checkBox[0].lastElementChild.checked = true;
+    categoryChecked[0] = true;
+    for (let i = 1; i < 5; i++) {
+      categoryChecked[i] = false;
+      checkBox[i].lastElementChild.checked = false;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    categoryChecked[i] = checkBox[i].lastElementChild.checked;
+  }
+  drinkPrintExec();
 });
