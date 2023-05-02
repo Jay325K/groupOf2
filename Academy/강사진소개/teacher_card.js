7d9319f10778c54aa989438e6a18791ca5cae64b
lyd1040 <85824033+lyd1040@users.noreply.github.com>
2023-05-03 01:10:53 +0900

    update
    시설소개 페이지 (추가)

--- Academy/강사진소개/teacher_card.js
+++ Academy/강사진소개/teacher_card.js
@@ -1,5 +1,5 @@
-$(function(){
+/* $(function(){
     $('.imgbox').on('click',function(){
         $(this).next().children('.trainer_info_wrap').toggleClass('active');
     });
-});
+}); */
---
+++ Academy/시설소개/space_intro_lnb.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.guide_wrap>ul>li>a').on('click focus',function(){
+    $('.guide_wrap>ul>li>ul').removeClass('active');
+    $(this).next().toggleClass('active');
+  })
+
+  
+})
