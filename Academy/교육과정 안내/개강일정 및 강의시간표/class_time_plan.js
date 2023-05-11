1921d35b50b6e79b865f881a870dc9f8678b51ac
chocoro99 <lie202319@naver.com>
2023-05-11 17:43:26 +0900

    feat : 폴더 정리

--- Academy/교육과정 안내/개강일정 및 강의시간표/class_time_plan.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.plan li a').on('click focus',function(){
-        $('.plan li a').next().removeClass('active');
-        $(this).next().addClass('active');
-    });
-});
--- Academy/아카데미소개/오시는길/Academy_lnb.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('.guide_wrap>ul>li>a').on('click focus',function(){
-    $('.guide_wrap>ul>li>ul').removeClass('active');
-    $(this).next().toggleClass('active');
-  })
-})
