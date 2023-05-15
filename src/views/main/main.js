30c36b5c66533419a665ff58e380b5b6c2619326
jay <hjysjhhj277@gmail.com>
2023-05-15 17:25:33 +0900

    test0515

---
+++ src/views/main/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    // let valY = window.pageYOffset;
+    // window.addEventListener('scroll',function(){
+    //     let valY_change = window.pageYOffset;
+    //     if(valY_change>valY){
+    //         $("#main3").css("top","0");
+    //     }
+        
+    // })
+
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true,
+        navigation:true,    
+        navigationPosition:'right'
+    });
+});
