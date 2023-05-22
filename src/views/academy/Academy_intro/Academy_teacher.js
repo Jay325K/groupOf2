b4a0b6320e9802f5643c56d07f1fc9fef12f3e5a
lyd1040 <skswngussla@naver.com>
2023-05-22 13:22:11 +0900

    save

--- src/views/academy/Academy_intro/Academy_teacher.js
+++ src/views/academy/Academy_intro/Academy_teacher.js
@@ -12,6 +12,7 @@ $(function(){
                 let this_idx=$(this).index();
                 //클릭시 작은 이미지 주소 저장
                 let img_src = $(this).children().children().children().eq(0).attr('src');
+                let img_alt = $(this).children().children().children().eq(0).attr('alt');
                 
                 $('.card .say_it').removeClass('active');
                 $(this).children('.say_it').addClass('active');
@@ -19,12 +20,13 @@ $(function(){
                 /* 작은 이미지의 주소를 프로필 이미지에 저장  0.15초 뒤 실행*/
                 setTimeout(() => {
                     $('.teacher_profile .show_teacher_img img').attr('src',img_src).animate({opacity:'1'},150);
-                $('.info .teacher_name').text(teacher_infomation[this_idx].name).animate({opacity:'1'},150);
-                $('.info .teacher_locate').text(teacher_infomation[this_idx].locate).animate({opacity:'1'},150);
-                $('.info .teacher_small_school').text(teacher_infomation[this_idx].small_school).animate({opacity:'1'},150);
-                $('.info .teacher_middle_school').text(teacher_infomation[this_idx].middle_school).animate({opacity:'1'},150);
-                $('.info .teacher_high_school').text(teacher_infomation[this_idx].high_school).animate({opacity:'1'},150);
-                $('.info .teacher_college').text(teacher_infomation[this_idx].college).animate({opacity:'1'},150);
+                    $('.teacher_profile .show_teacher_img img').attr('alt',img_alt).animate({opacity:'1'},150);
+                    $('.info .teacher_name').text(teacher_infomation[this_idx].name).animate({opacity:'1'},150);
+                    $('.info .teacher_locate').text(teacher_infomation[this_idx].locate).animate({opacity:'1'},150);
+                    $('.info .teacher_small_school').text(teacher_infomation[this_idx].small_school).animate({opacity:'1'},150);
+                    $('.info .teacher_middle_school').text(teacher_infomation[this_idx].middle_school).animate({opacity:'1'},150);
+                    $('.info .teacher_high_school').text(teacher_infomation[this_idx].high_school).animate({opacity:'1'},150);
+                    $('.info .teacher_college').text(teacher_infomation[this_idx].college).animate({opacity:'1'},150);
                 }, 150);
                 /* 투명하게 만듬 */
                 $('.teacher_profile .show_teacher_img img').animate({opacity:'0'},150);
