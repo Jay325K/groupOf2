8703bab124a8fa41a7b32d1f23b9491225a9368b
lyd1040 <skswngussla@naver.com>
2023-05-22 14:31:55 +0900

    save

--- src/views/footer_header/header.js
+++ src/views/footer_header/header.js
@@ -5,10 +5,10 @@ $(function(){
 
         if(scroll>0){
             $('.header').addClass('active');
-            $('.header_first').addClass('active');
+            $('.header_first').addClass('active'); 
         }else{
             $('.header').removeClass('active');
-            $('.header_first').removeClass('active');
+            $('.header_first').removeClass('active'); 
         }
     });
 
@@ -18,5 +18,21 @@ $(function(){
     });
     $('.gnb').mouseleave(function(){
         $('.depth-02').hide();
+    })
+    window.addEventListener('resize',function(){
+        if(window.innerWidth<1920 && window.innerWidth>1200){
+            
+                let window_width = this.innerWidth;
+                let background_move_locate = document.querySelector('.depth-02');
+                console.log('window-width = ' + window_width);
+                console.log(', (background_move_locate.clientWidth+720) =' + (background_move_locate.clientWidth+720));
+
+                console.log(', result = =' +((background_move_locate.clientWidth+720) - window_width));
+                console.log(', result = =' +(window_width-(background_move_locate.clientWidth+720)));
+                
+                background_move_locate.style.backgroundPosition=(0+(window_width-(1920))) +'px 100%'; 
+            
+            
+        }
     })
 });
