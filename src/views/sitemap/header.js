2c39b71a6ea4817c64dae4aef10d95dff7a4f373
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-05-24 16:21:22 +0900

    footer header sitemap
    footer header sitemap

---
+++ src/views/sitemap/header.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $.ajax({
+        url: '/src/views/footer_header/header.html',
+        dataType: 'html',
+        success: function(html){
+            $('#header').html(html);
+
+            $.getScript({
+                url: '/src/views/footer_header/header.js',
+                success: function(){}
+            })
+        }
+    });
+
+    $.ajax({
+        url: '/src/views/footer_header/footer.html',
+        dataType: 'html',
+        success: function(html){
+            $('#footer').html(html);
+        }
+    });
+})
