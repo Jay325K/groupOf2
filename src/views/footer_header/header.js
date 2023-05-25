ce81f249753573c3d5c2db8b12bc1b744afcc0d0
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-05-25 13:58:50 +0900

    header slide effect
    header slide effect

--- src/views/footer_header/header.js
+++ src/views/footer_header/header.js
@@ -11,12 +11,19 @@ $(function(){
     });
 
     /***   depth-02   ***/
-    $('.gnb').mouseenter(function(){
-        $('.depth-02').show();
-    });
-    $('.gnb').mouseleave(function(){
-        $('.depth-02').hide();
+    $('.gnb').hover(function(){
+        $('.depth-02>div').addClass('active');
+    },function(){
+        $('.depth-02>div').removeClass('active');
     })
+
+    // $('.gnb').hover(function(){
+    //         $('.depth-02').css({display:'block', height:'0'}).stop().animate({height:"200px"}, "fast");
+    // },function(){
+    //     $('.depth-02').stop().animate({height:'0'}, 'fast', function(){
+    //         $('.depth-02').css({display:'none'})
+    //     });
+    // });
 
     window_resize();
     window.addEventListener('resize',function(){
@@ -39,7 +46,7 @@ function window_resize(){
     if(window.innerWidth<1920 && window.innerWidth>1050){
             
                 let window_width = this.innerWidth;
-                let background_move_locate = document.querySelector('.depth-02');
+                let background_move_locate = document.querySelector('.depth-02>div');
                 
                 background_move_locate.style.backgroundPosition=(130+(window_width-(1920))) +'px 100%'; 
             
